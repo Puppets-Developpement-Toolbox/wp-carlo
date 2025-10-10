1b48751d38c873722fb8f101c4244a56c0c39aef
PixelPoetryFR <julien@pixelpoetry.fr>
2025-10-10 09:37:58 +0200

    upd singular.php

--- singular.php
+++ singular.php
@@ -1,10 +1,10 @@
 <?php
 ob_start();
 $template = match (true) {
-    is_home() => "archive",
-    is_404() => "error",
-    is_page() => get_page_template_slug() ?: "default",
-    is_single() => "type_" . get_post_type(),
+  is_home() => "archive",
+  is_404() => "error",
+  is_page() => get_page_template_slug() ?: "default",
+  is_single() => "type_" . get_post_type(),
 };
 
 do_action("carlo_prerender", $template);
@@ -13,22 +13,21 @@ carlo_render("global/html_start");
 #carlo_render("global/header");
 
 if(is_page()) {
-    $regions = carlo_structure("templates")[$template];
+  $regions = carlo_structure("templates")[$template];
 } elseif(!is_404()) {
-    $regions = carlo_structure("types")[get_post_type()]['template'];
+  $regions = carlo_structure("types")[get_post_type()]['template'];
 }
-    ?>
-<main id="main" class="page-content flex-1">
-<?php
-    if(!empty($regions)){
-        foreach ($regions as $region => $sections) {
-              if(!str_starts_with($region, '_')) {
-                carlo_render_region($template, $region);
-              }
-          }
-    }
 ?>
-
+<main id="main" class="flex-1">
+<?php
+  if(!empty($regions)){
+    foreach ($regions as $region => $sections) {
+      if(!str_starts_with($region, '_')) {
+        carlo_render_region($template, $region);
+      }
+    }
+  }
+?>
 </main>
 <?php
 carlo_render('global/html_end');
