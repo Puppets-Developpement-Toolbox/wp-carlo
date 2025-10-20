c2e1a9ce17818dc165dd038195182a451e54e4e9
PixelPoetryFR <julien@pixelpoetry.fr>
2025-10-20 09:32:18 +0200

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'puppets-wp-themes/carlo',
+        'pretty_version' => '3.x-dev',
+        'version' => '3.9999999.9999999.9999999-dev',
+        'reference' => '65bef2caedca305efca346e6326c8092e1ad5742',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'puppets-wp-themes/carlo' => array(
+            'pretty_version' => '3.x-dev',
+            'version' => '3.9999999.9999999.9999999-dev',
+            'reference' => '65bef2caedca305efca346e6326c8092e1ad5742',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
