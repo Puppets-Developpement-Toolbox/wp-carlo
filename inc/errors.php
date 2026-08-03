2345d75220210cecdfba31fdc53a68ef373e13b5
Samuel Laulhau <samuel.laulhau@puppets.fr>
2026-08-03 17:10:59 +0200

    Enhance debug condition for Symfony error handler

--- inc/errors.php
+++ inc/errors.php
@@ -1,6 +1,6 @@
 <?php
 use Symfony\Component\ErrorHandler\Debug;
 
-if (WP_DEBUG) {
+if (WP_DEBUG && WP_DEBUG_DISPLAY && class_exists('Symfony\Component\ErrorHandler\Debug')) {
     Debug::enable();
 }
